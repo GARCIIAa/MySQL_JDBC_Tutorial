9536068870c3463ab21d899ec70e6133c404dc6f
GARCIIAa <15301164@bjtu.edu.cn>
2017-04-22 16:38:10 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,13 +5,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		// queryData();
-		//updateData();
-		
+
+		// updateData();
+
+		int id = insertCandidate("Bush", "Lily", Date.valueOf("1980-01-04"),
+				"bush.l@yahoo.com", "(408) 898-6666");
+		System.out.println(String.format(
+				"A new candidate with id %d has been inserted.", id));
 	}
 
 	public static void queryData() {
@@ -65,5 +72,56 @@ public class Main {
 		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
+	}
+
+	/**
+	 * Insert a new candidate
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param dob
+	 * @param email
+	 * @param phone
+	 * @return
+	 */
+	public static int insertCandidate(String firstName, String lastName,
+			Date dob, String email, String phone) {
+		// for insert a new candidate
+		ResultSet rs = null;
+		int candidateId = 0;
+
+		String sql = "INSERT INTO candidates(first_name,last_name,dob,phone,email) "
+				+ "VALUES(?,?,?,?,?)";
+
+		try (Connection conn = MySQLJDBCUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql,
+						Statement.RETURN_GENERATED_KEYS);) {
+
+			// set parameters for statement
+			pstmt.setString(1, firstName);
+			pstmt.setString(2, lastName);
+			pstmt.setDate(3, dob);
+			pstmt.setString(4, phone);
+			pstmt.setString(5, email);
+
+			int rowAffected = pstmt.executeUpdate();
+			if (rowAffected == 1) {
+				// get candidate id
+				rs = pstmt.getGeneratedKeys();
+				if (rs.next())
+					candidateId = rs.getInt(1);
+
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return candidateId;
 	}
 }
