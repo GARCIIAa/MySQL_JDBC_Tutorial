e10940aa4f0e329e541430516231009c11c72d1e
GARCIIAa <15301164@bjtu.edu.cn>
2017-04-22 15:06:50 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,28 @@
 package mysqltutorial;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
  
-/**
- *
- * @author mysqltutorial.org
- */
 public class Main {
- 
-    public static void main(String[] args) {
-        // create a new connection from MySQLJDBCUtil
-        try (Connection conn = MySQLJDBCUtil.getConnection()) {
-            
-            // print out a message
-            System.out.println(String.format("Connected to database %s "
-                    + "successfully.", conn.getCatalog()));
+    
+    public static void main(String[] args) { 
+    
+        String sql = "SELECT first_name, last_name, email " +
+                     "FROM candidates";
+        
+        try (Connection conn = MySQLJDBCUtil.getConnection();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)) {
+           
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("first_name") + "\t" + 
+                                   rs.getString("last_name")  + "\t" +
+                                   rs.getString("email"));
+                    
+            }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
